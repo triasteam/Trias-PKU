9ec84cfac33648c80b9f8a0dfbb4a300a9040952
sacrohu <sacrohu@126.com>
2019-08-24 22:50:19 +0800

    add trias

---
+++ StealthAddress/trias/shield_test.go
@@ -0,0 +1,26 @@
+package stealthaddress
+
+import (
+    "crypto/ecdsa"
+    "crypto/rand"
+    "fmt"
+    "testing"
+)
+
+//generate public and private key pairs
+var privateView, _ = ecdsa.GenerateKey(curveP256, rand.Reader)
+var privateSpend, _ = ecdsa.GenerateKey(curveP256, rand.Reader)
+
+func TestCreateVerifyShieldAddr(test *testing.T) {
+    publicView := privateView.Public().(*ecdsa.PublicKey)
+    publicSpend := privateSpend.Public().(*ecdsa.PublicKey)
+    publicShieldP, publicShieldR := CreateShieldAddr(publicView, publicSpend)
+    isAddress := VerifyShieldAddr(privateView, publicSpend, publicShieldP, publicShieldR)
+    privateStealth := GetprivateStealth(privateView, privateSpend, publicShieldR)
+
+    fmt.Println("View ", privateView.D.Bytes(), "\n", publicView)
+    fmt.Println("Spend", privateSpend.D.Bytes(), "\n", publicSpend)
+    fmt.Println("Stealth", publicShieldP, "\nR", publicShieldR)
+    fmt.Println(isAddress)
+    fmt.Println("Stealth", privateStealth)
+}
